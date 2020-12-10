846c7eb5e2da7a3371eb3d0344a5f2376ef1b90a
Oscar Lara <olara@csumb.edu>
2020-12-10 02:04:14 -0800

    connected parse to database, working sign up and login

--- journal/AppDelegate.swift
+++ journal/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "Q4mDNPipdlfjwcwPzL3AJuyoCXHwB474FQQpeZth" //app id
+            $0.clientKey = "6OMpKvCrNG8zgBeyNfDa8miN95fBYzWFAMYzylCb" // client key
+            $0.server = "https://parseapi.back4app.com" //
+        }
+        
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
